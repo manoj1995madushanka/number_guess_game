f0a2d977d357c554edaf32a7856ecd49cfb0dc59
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-03-19 08:42:43 +0530

    create game screens

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,10 @@
+import {TextInput,View} from 'react-native';
+
+function StartGameScreen(){
+    return <View>
+        <TextInput/>
+        buttons 
+    </View>
+}
+
+export default StartGameScreen;
