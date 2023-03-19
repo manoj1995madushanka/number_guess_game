b06f8e9bb017aca51ebf048442e40138463b129c
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-03-19 14:23:57 +0530

    add styles to text input field

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,9 @@
-import { StyleSheet,TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen() {
     return <View style={styles.inputContainer}>
-        <TextInput />
+        <TextInput style={styles.numberInput} maxLength={2}/>
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -13,7 +13,7 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer: {
-       // flex: 1,
+        // flex: 1,
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
@@ -25,5 +25,16 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 6,
         shadowOpacity: 0.25
+    },
+    numberInput: {
+        height: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        width:50,
+        textAlign:'center',
     }
 })
