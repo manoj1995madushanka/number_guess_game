490db5804e68af26a049fa7beecce3307a6971df
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-03-19 22:13:26 +0530

    add opacity to background image

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
-import {LinearGradient} from 'expo-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
-      <ImageBackground source={require('./assets/images/background.png')} resizeMode="cover" style={styles.rootScreen}>
-        	<StartGameScreen/>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground source={require('./assets/images/background.png')}
+        resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backgraoundImage}>
+        <StartGameScreen />
       </ImageBackground>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  rootScreen:{
+  rootScreen: {
     //backgroundColor:'#ddb52f',
-    flex:1,
+    flex: 1,
+  },
+  backgraoundImage:{
+    opacity:0.15,
   }
 });
