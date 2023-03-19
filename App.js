13cad12e026ce54d7054fab86096c0563d002063
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-03-19 21:07:57 +0530

    add background color

--- App.js
+++ App.js
@@ -4,10 +4,15 @@ import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen/>
+    <View style={styles.rootScreen}>
+      <StartGameScreen/>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  rootScreen:{
+    backgroundColor:'#ddb52f',
+    flex:1,
+  }
 });
