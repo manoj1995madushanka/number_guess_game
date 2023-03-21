6b67d0ae436f0e8d5d8e724f06ae30fba136f0e7
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-03-21 22:03:04 +0530

    create common module for generate title

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,9 @@
 import {Text,View,StyleSheet,SafeAreaView} from 'react-native';
+import Title from '../components/Title';
 
 function GameScreen(){
     return <View style={styles.screen}>
-        <Text style={styles.title}>Opponent's Guess</Text>
+        <Title>Opponent's Guess</Title>
         {/*Guess*/}
         <View>
             <Text>Higher or lower?</Text>
@@ -19,13 +20,5 @@ const styles = StyleSheet.create({
         flex:1,
         padding:24,
     },
-    title:{
-        fontSize:24,
-        fontWeight:'bold',
-        color:'#ddb52f',
-        textAlign:'center',
-        borderWidth:2,
-        borderColor:'#ddb52f',
-        padding:12,
-    }
+    
 });
