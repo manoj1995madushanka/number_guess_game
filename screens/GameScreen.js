896318f812d574523cb77e123aac23fb621c8f6e
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-03-21 21:55:13 +0530

    create title

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,8 @@
-import {Text,View,StyleSheet} from 'react-native';
+import {Text,View,StyleSheet,SafeAreaView} from 'react-native';
 
 function GameScreen(){
     return <View style={styles.screen}>
-        <Text>Opponent's Guess</Text>
+        <Text style={styles.title}>Opponent's Guess</Text>
         {/*Guess*/}
         <View>
             <Text>Higher or lower?</Text>
@@ -17,6 +17,15 @@ export default GameScreen;
 const styles = StyleSheet.create({
     screen:{
         flex:1,
+        padding:24,
+    },
+    title:{
+        fontSize:24,
+        fontWeight:'bold',
+        color:'#ddb52f',
+        textAlign:'center',
+        borderWidth:2,
+        borderColor:'#ddb52f',
         padding:12,
     }
 });
